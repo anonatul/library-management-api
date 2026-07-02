61872b9dfd374c2cf5158a7fa5d2af9e61982961
Atul <atul.kevat28@gmail.com>
2026-07-02 21:37:22 +0530

    add user login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,7 +11,6 @@ export const registerUser = async (req, res) => {
             message: "All fields are required"
         });
     };
-
     
     try {
         
@@ -59,4 +58,65 @@ export const registerUser = async (req, res) => {
             message: "Server error"
         });
     };
-}
+};
+
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    // checking for all required fields
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: "All fields are required"
+        });
+    };
+
+    try {
+        // checking if user exists
+        const user = await User.findOne({
+            email: email.toLowerCase()
+        });
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid credentials"
+            });
+        };
+
+        // checking if password is correct or not
+        const isCorrectPassword = await user.comparePassword(password);
+        
+        if (!isCorrectPassword) {
+             return res.status(401).json({
+                success: false,
+                message: "Invalid credentials"
+            });
+        };
+
+        // generating token for the user
+        const token = generateToken(user._id);
+
+        res.status(200).json({
+            success: true,
+            message: "User logged in successfully", 
+            data: {
+                user: {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    profilePicture: user.profilePicture,
+                    role: user.role,
+                    createdAt: user.createdAt
+                },
+                token: token
+            }
+        });
+    } catch (error) {
+        console.error(`Error logging in user: ${error.message}`);
+        res.status(500).json({
+            success: false,
+            message: "Server error"
+        });
+    }
+};
