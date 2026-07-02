a69e28d7394348465fe86aed7252b6b847f3cb18
Atul <atul.kevat28@gmail.com>
2026-07-02 20:34:16 +0530

    add user registeration

---
+++ src/controllers/authController.js
@@ -0,0 +1,62 @@
+import User from "../models/User.js";
+import { generateToken } from "../utils/jwt.js";
+
+export const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    // checking for all required fields
+    if (!name || !email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: "All fields are required"
+        });
+    };
+
+    
+    try {
+        
+        // checking if user already exists
+        const userExists = await User.findOne({
+            email: email.toLowerCase()
+        });
+    
+        if (userExists) {
+            return res.status(409).json({
+                success: false,
+                message: "User already exists"
+            });
+        };
+
+        // creating new user in db
+        const user = await User.create({
+            name: name,
+            email: email,
+            password: password
+        });
+
+        // generating token for the new user
+        const token = generateToken(user._id);
+
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+            data: {
+                user: {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    profilePicture: user.profilePicture,
+                    role: user.role,
+                    createdAt: user.createdAt
+                },
+                token: token
+            }
+        });
+    } catch (error) {
+        console.error(`Error registering user: ${error.message}`);
+        res.status(500).json({
+            success: false,
+            message: "Server error"
+        });
+    };
+}
